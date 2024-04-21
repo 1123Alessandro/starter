7e98967b4c0d4db4f6e893d0812cf3a26b0d9723
1123Alessandro <alessandroandyrei@gmail.com>
2024-04-22 04:27:41 +0800

    import most settings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,8 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>of", "<cmd>let @+ = expand('%')<cr>")
+map("n", "<leader>od", "<cmd>let @+ = expand('%:h')<cr>")
+map("n", "<leader>fd", "<cmd>let @+ = expand('%:p:h')<cr>")
+map("n", "<leader>ob", "<cmd>only<cr>")
