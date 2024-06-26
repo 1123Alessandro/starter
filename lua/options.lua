1240ac08dd99e7455c60bc18baffa142ea5c5cff
1123Alessandro <alessandroandyrei@gmail.com>
2024-06-26 13:24:28 +0800

    tab size and lsp reqs

--- lua/options.lua
+++ lua/options.lua
@@ -6,7 +6,10 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 vim.wo.relativenumber = true
--- vim.opt.conceallevel = 1
+vim.opt.conceallevel = 1
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 
 -- The below settings make Leap's highlighting closer to what you've been
 -- used to in Lightspeed.
