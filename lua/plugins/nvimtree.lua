ef32a01edc96dde88d66893ab4419d4c19bac293
1123Alessandro <alessandroandyrei@gmail.com>
2024-10-14 22:34:32 +0800

    FEAT: me v2

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,13 +1,11 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
-    config = function()
-      require("nvim-tree").setup({
-        git = {
-          timeout = 5000,
-        },
-      })
-    end,
+    opts = {
+      git = {
+        timeout = 5000,
+      },
+    },
     keys = {
       { "<leader>e", "<cmd>NvimTreeFindFile<cr>", desc = "nvimtree focus on file" },
       { "<C-n>", "<cmd>NvimTreeToggle<cr>", desc = "nvimtree toggle"}
